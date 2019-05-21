6e4f440c05717efa3e16733bcd7dd0db3a5214ad
Heasley <heasleys4hemp@hotmail.com>
2019-05-21 03:18:28 -0400

    Testing api stuff

--- welcome.php
+++ welcome.php
@@ -10,16 +10,26 @@ $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($c
 $row = mysqli_fetch_assoc($resultset);
 include('navbar.php');
 ?>
-<div class="container">    
+<div class="container">
 	<div class='alert alert-success'>
 		<button class='close' data-dismiss='alert'>&times;</button>
 		Hello, <br><br>Welcome to the members page.<br><br>
-    </div>		
+    </div>
 </div>
 <?php
 
-$jsonurl = "https://api.torn.com/faction/13784?selections=basic&key=jIirMCNvK8q2hf8u";
+//$jsonurl = "https://api.torn.com/faction/13784?selections=basic&key=jIirMCNvK8q2hf8u";
+$jsonurl = "https://api.torn.com/user/2169837?selections=timestamp,networth,bazaar,display,inventory,hof,travel,education,medals,honors,notifications,personalstats,workstats,crimes,icons,cooldowns,money,perks,battlestats,bars,profile,basic,stocks,properties,jobpoints,merits,refills,discord,gym&key=jIirMCNvK8q2hf8u";
    $json = file_get_contents($jsonurl); //gets output of API
+
+$data = json_decode($json, true);
+
+
+echo '<p>' . $data["name"] . ' [' . $data["player_id"] . ']<p/>';
+echo '<p>' . $data["energy"]["current"].'/'.$data["energy"]["maximum"].'</p>';
+
+
+/*
    $decodedString =  new RecursiveIteratorIterator ( new RecursiveArrayIterator(json_decode($json, true)), RecursiveIteratorIterator::SELF_FIRST); //parses API JSON output
 echo "<table border='1'><tr>";
 foreach($decodedString as $key=>$value) {
@@ -44,6 +54,7 @@ function printValues($arr) {
     function else add the value found to the output items array,
     and increment counter by 1 for each value found
     */
+		/*
     foreach($arr as $key=>$value){
         if(is_array($value)){
             printValues($value);
@@ -55,7 +66,7 @@ function printValues($arr) {
 
     // Return total count and values found in array
     return array('total' => $count, 'values' => $values);
-}
+}*/
 
 include('footer.php');
 ?>
