8544a08d321c9dae280a53bd451cc2d57f854020
Heasley <heasleys4hemp@hotmail.com>
2019-05-21 06:21:24 -0400

    Added faction page Added faction link to navbar

---
+++ faction.php
@@ -0,0 +1,84 @@
+<?php
+include('header.php');
+include('navbar.php');
+
+
+$action = htmlspecialchars($_GET["action"]);
+
+//something for later, idk just an idea
+switch ($action) {
+    case "energy":
+
+    break;
+    case "":
+    //
+    break;
+    default:
+    //
+    break;
+    }
+
+}
+
+
+?>
+
+
+
+<div class="container">
+  <div class="row">
+   <div class="col-md-3"></div>
+   <div class="col-md-6 border border-dark shadow py-4 mt-4 rounded">
+
+
+<table class="table table-hover table-striped table-dark">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col">Player</th>
+      <th scope="col">Energy</th>
+      <th scope="col">Drug Cooldown</th>
+      <th scope="col">Booster Cooldown</th>
+      <th scope="col">Energy Refill Used</th>
+      <th scope="col">Nerve Refill Used</th>
+    </tr>
+  </thead>
+  <tbody>
+
+<?php
+
+
+$apikey = "UtuyM2roWM6vDjKj"; //Heasleys4hemp's apikey
+//$jsonurl = "https://api.torn.com/faction/13784?selections=basic&key=jIirMCNvK8q2hf8u";
+$jsonurl = "https://api.torn.com/user/?selections=timestamp,networth,bazaar,display,inventory,hof,travel,education,medals,honors,notifications,personalstats,workstats,crimes,icons,cooldowns,money,perks,battlestats,bars,profile,basic,stocks,properties,jobpoints,merits,refills,discord,gym&key=" . $apikey;
+   $json = file_get_contents($jsonurl); //gets output of API
+
+$data = json_decode($json, true);
+
+//could do a for loop here later, but only data we have is mine so weeeeee
+echo '<tr>';
+echo '<th scope="row">' . $data["name"] . ' [' . $data["player_id"] . ']</th>';
+echo '<td>' . $data["energy"]["current"].'/'.$data["energy"]["maximum"] . '</td>';
+echo '<td>' . gmdate("H:i:s",$data["cooldowns"]["drug"])  . '</td>';
+echo '<td>' . gmdate("H:i:s",$data["cooldowns"]["booster"]) . '</td>';
+echo '<td>' . $data["refills"]["energy_refill_used"] . '</td>';
+echo '<td>' . $data["refills"]["energy_refill_used"] . '</td>';
+echo '</tr>';
+
+?>
+
+
+  </tbody>
+</table>
+
+
+  </div>
+ <div class="col-md-3"></div>
+</div>
+</div>
+
+
+
+
+<?php
+include('footer.php');
+?>
--- navbar.php
+++ navbar.php
@@ -4,10 +4,10 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMobileDropdown" aria-controls="navMobileDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    
-    
+
+
     <div class="collapse navbar-collapse" id="navMobileDropdown">
-  
+
     <div class="container d-flex justify-content-lg-end pr-5">
      <ul class="navbar-nav pr-3">
        <li class="nav-item active">
@@ -16,10 +16,16 @@
        <li class="nav-item active">
          <a class="nav-item nav-link" href="#">Placeholder</a>
        </li>
-       <li class="nav-item active">
-         <a class="nav-item nav-link" href="#">Placeholder</a>
-       </li>
-      
+       <li class="nav-item active dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navFactionDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Faction
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navFactionDropdown">
+          <a class="dropdown-item" href="faction.php"><i class="fas fa-users"></i> Faction</a>
+          <a class="dropdown-item" href="faction.php?action=energy"><i class="fas fa-battery-half"></i> Energy</a>
+        </div>
+      </li>
+
        <li class="nav-item active dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navProfileDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <?php echo $row['username']; ?>
@@ -31,11 +37,11 @@
           <a class="dropdown-item" href="logout.php"><i class="fas fa-sign-out-alt"></i> Sign Out</a>
         </div>
       </li>
-      
-      
+
+
      </ul>
     </div>
-    
+
     </div>
 
 </nav>
