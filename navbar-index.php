954663f82ac2e04e7694c82c2f80c3de046c6dc5
Heasley <heasleys4hemp@hotmail.com>
2019-05-21 00:44:58 -0400

    update navbar-index to not be old
    old navbar was using old bootstrap. simple copy paste placeholder for now

--- navbar-index.php
+++ navbar-index.php
@@ -1,25 +1,41 @@
-<body class="">
-<div role="navigation" class="navbar navbar-default navbar-static-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          Torn-a-new-Hole
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand pl-5" href="#">Warbirds</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMobileDropdown" aria-controls="navMobileDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    
+    <div class="collapse navbar-collapse" id="navMobileDropdown">
+  
+    <div class="container d-flex justify-content-lg-end pr-5">
+     <ul class="navbar-nav pr-3">
+       <li class="nav-item active">
+         <a class="nav-item nav-link" href="#">Home</a>
+       </li>
+       <li class="nav-item active">
+         <a class="nav-item nav-link" href="#">Placeholder</a>
+       </li>
+       <li class="nav-item active">
+         <a class="nav-item nav-link" href="#">Placeholder</a>
+       </li>
+      
+       <li class="nav-item active dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navProfileDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Placeholder
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navProfileDropdown">
+          <a class="dropdown-item" href="#"><i class="fas fa-user"></i> View Profile</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Placeholder</a>
+          
         </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-           
-          </ul>
-         
-        </div><!--/.nav-collapse -->
-      </div>
+      </li>
+      
+      
+     </ul>
     </div>
-	
-	<div class="container" style="min-height:500px;">
-	<div class=''>
-	</div>
+    
+    </div>
+
+</nav>
