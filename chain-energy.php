568823aa373f9388de5ee18f80ec266ffcc40d53
Heasley <heasleys4hemp@hotmail.com>
2019-05-23 20:59:49 -0400

    more edits to file inputs

--- chain-energy.php
+++ chain-energy.php
@@ -18,43 +18,45 @@ include('navbar.php');
     			  <div class="card-body">
 
     			   <h5 class="title">Energy Reports: Start of Chain</h5>
-
-             <div class="custom-file">
-                <input type="file" class="custom-file-input" id="strengthSVG_start">
-                <label class="custom-file-label" for="strengthSVG_start">Choose file</label>
+        <form action="" method="post" enctype="multipart/form-data">
+             <div class="custom-file pb-2">
+                <input type="file" class="custom-file-input" name="strengthSVG_start">
+                <label class="custom-file-label" for="strengthSVG_start">Strength SVG</label>
              </div>
-             <div class="custom-file">
-                <input type="file" class="custom-file-input" id="speedSVG_start">
-                <label class="custom-file-label" for="speedSVG_start">Choose file</label>
+             <div class="custom-file pb-2">
+                <input type="file" class="custom-file-input" name="speedSVG_start">
+                <label class="custom-file-label" for="speedSVG_start">Speed SVG</label>
              </div>
-             <div class="custom-file">
-                <input type="file" class="custom-file-input" id="defenseSVG_start">
-                <label class="custom-file-label" for="defenseSVG_start">Choose file</label>
+             <div class="custom-file pb-2">
+                <input type="file" class="custom-file-input" name="defenseSVG_start">
+                <label class="custom-file-label" for="defenseSVG_start">Defense SVG</label>
              </div>
-             <div class="custom-file">
-                <input type="file" class="custom-file-input" id="dexteritySVG_start">
-                <label class="custom-file-label" for="dexteritySVG_start">Choose file</label>
+             <div class="custom-file pb-2">
+                <input type="file" class="custom-file-input" name="dexteritySVG_start">
+                <label class="custom-file-label" for="dexteritySVG_start">Dexterity SVG</label>
              </div>
+         </form>
              <hr/>
              <h5 class="title">Energy Reports: End of Chain</h5>
-
-             <div class="custom-file">
-                <input type="file" class="custom-file-input" id="strengthSVG_end">
-                <label class="custom-file-label" for="strengthSVG_end">Choose file</label>
+         <form action="" method="post" enctype="multipart/form-data">
+             <div class="custom-file pb-2">
+                <input type="file" class="custom-file-input" name="strengthSVG_end">
+                <label class="custom-file-label" for="strengthSVG_end">Strength SVG</label>
              </div>
-             <div class="custom-file">
-                <input type="file" class="custom-file-input" id="speedSVG_end">
-                <label class="custom-file-label" for="speedSVG_end">Choose file</label>
+             <div class="custom-file pb-2">
+                <input type="file" class="custom-file-input" name="speedSVG_end">
+                <label class="custom-file-label" for="speedSVG_end">Speed SVG</label>
              </div>
-             <div class="custom-file">
-                <input type="file" class="custom-file-input" id="defenseSVG_end">
-                <label class="custom-file-label" for="defenseSVG_end">Choose file</label>
+             <div class="custom-file pb-2">
+                <input type="file" class="custom-file-input" name="defenseSVG_end">
+                <label class="custom-file-label" for="defenseSVG_end">Defense SVG</label>
              </div>
-             <div class="custom-file">
-                <input type="file" class="custom-file-input" id="dexteritySVG_end">
-                <label class="custom-file-label" for="dexteritySVG_end">Choose file</label>
+             <div class="custom-file pb-2">
+                <input type="file" class="custom-file-input" name="dexteritySVG_end">
+                <label class="custom-file-label" for="dexteritySVG_end">Dexterity SVG</label>
              </div>
-
+         </form>
+             <button type="submit" class="btn btn-primary" name="importSubmit" value="IMPORT">Submit</button>
     			  </div>
     			</div>
     		 </div> <!-- col -->
