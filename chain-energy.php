fc6361cd4f4eacacc135462078d30b4be8fa2f82
Heasley <heasleys4hemp@hotmail.com>
2019-05-23 21:09:22 -0400

    fixed margins, testing centerness

--- chain-energy.php
+++ chain-energy.php
@@ -12,7 +12,7 @@ include('navbar.php');
 
     	<div class="row pb-3">
 
-    		 <div class="col-lg-4 col-md-6 pt-3">
+    		 <div class="col-lg-4 col-md-6 pt-3 mx-auto">
     			<div class="card border border-dark shadow rounded">
     			  <h5 class="card-header">Energy Contributions</h5>
     			  <div class="card-body">
@@ -20,36 +20,31 @@ include('navbar.php');
     			   <h5 class="title">Energy Reports: Start of Chain</h5>
         <form action="" method="post" enctype="multipart/form-data">
 
-             <div class="pb-2">
-             <div class="custom-file pb-2">
+             <div class="custom-file mb-2">
                 <input type="file" class="custom-file-input" name="strengthSVG_start">
                 <label class="custom-file-label" for="strengthSVG_start">Strength SVG</label>
              </div>
-             </div>
 
-             <div class="pb-2">
-             <div class="custom-file pb-2">
+             <div class="custom-file mb-2">
                 <input type="file" class="custom-file-input" name="speedSVG_start">
                 <label class="custom-file-label" for="speedSVG_start">Speed SVG</label>
              </div>
-             </div>
 
-             <div class="pb-2">
-             <div class="custom-file">
+             <div class="custom-file  mb-2">
                 <input type="file" class="custom-file-input" name="defenseSVG_start">
                 <label class="custom-file-label" for="defenseSVG_start">Defense SVG</label>
              </div>
-             </div>
 
-             <div class="pb-2">
-             <div class="custom-file pb-2">
+             <div class="custom-file mb-2">
                 <input type="file" class="custom-file-input" name="dexteritySVG_start">
                 <label class="custom-file-label" for="dexteritySVG_start">Dexterity SVG</label>
              </div>
-             </div>
 
          </form>
+
              <br/><hr/><br/>
+
+
              <h5 class="title">Energy Reports: End of Chain</h5>
          <form action="" method="post" enctype="multipart/form-data">
 
@@ -81,7 +76,7 @@ include('navbar.php');
       </div> <!-- row -->
 
       <div class="row">
-        <div class="col-lg-6 col-md-6 pt-3">
+        <div class="col-lg-6 col-md-6 pt-3 mx-auto">
           <div class="card border border-dark shadow rounded">
     			  <h5 class="card-header">Results</h5>
     			  <div class="card-body">
