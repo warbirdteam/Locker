9753acc030f45a2dec1ffdc6f7141cac742ffd3d
Heasley <heasleys4hemp@hotmail.com>
2019-05-23 20:49:52 -0400

    edit to file inputs

--- chain-energy.php
+++ chain-energy.php
@@ -12,39 +12,47 @@ include('navbar.php');
 
     	<div class="row pb-3">
 
-    		 <div class="col-lg-8 col-md-6 pt-3">
+    		 <div class="col-lg-6 col-md-6 pt-3">
     			<div class="card border border-dark shadow rounded">
     			  <h5 class="card-header">Energy Contributions</h5>
     			  <div class="card-body">
 
-    			   <p class="card-text"> Add Energy reports for each category.</p>
+    			   <h5 class="title">Energy Reports: Start of Chain</h5>
 
              <div class="custom-file">
-                <input type="file" class="custom-file-input" id="strengthSVG_first">
-                <label class="custom-file-label" for="strengthSVG_first">Choose file</label>
-
-                <input type="file" class="custom-file-input" id="speedSVG_first">
-                <label class="custom-file-label" for="speedSVG_first">Choose file</label>
-
-                <input type="file" class="custom-file-input" id="defenseSVG_first">
-                <label class="custom-file-label" for="defenseSVG_first">Choose file</label>
-
-                <input type="file" class="custom-file-input" id="dexteritySVG_first">
-                <label class="custom-file-label" for="dexteritySVG_first">Choose file</label>
+                <input type="file" class="custom-file-input" id="strengthSVG_start">
+                <label class="custom-file-label" for="strengthSVG_start">Choose file</label>
              </div>
+             <div class="custom-file">
+                <input type="file" class="custom-file-input" id="speedSVG_start">
+                <label class="custom-file-label" for="speedSVG_start">Choose file</label>
+             </div>
+             <div class="custom-file">
+                <input type="file" class="custom-file-input" id="defenseSVG_start">
+                <label class="custom-file-label" for="defenseSVG_start">Choose file</label>
+             </div>
+             <div class="custom-file">
+                <input type="file" class="custom-file-input" id="dexteritySVG_start">
+                <label class="custom-file-label" for="dexteritySVG_start">Choose file</label>
+             </div>
+             <hr/>
+             <h5 class="title">Energy Reports: End of Chain</h5>
 
              <div class="custom-file">
-                <input type="file" class="custom-file-input" id="strengthSVG_last">
-                <label class="custom-file-label" for="strengthSVG_last">Choose file</label>
-
-                <input type="file" class="custom-file-input" id="speedSVG_last">
-                <label class="custom-file-label" for="speedSVG_last">Choose file</label>
-
-                <input type="file" class="custom-file-input" id="defenseSVG_last">
-                <label class="custom-file-label" for="defenseSVG_last">Choose file</label>
-
-                <input type="file" class="custom-file-input" id="dexteritySVG_last">
-                <label class="custom-file-label" for="dexteritySVG_last">Choose file</label>
+                <input type="file" class="custom-file-input" id="strengthSVG_end">
+                <label class="custom-file-label" for="strengthSVG_end">Choose file</label>
+             </div>
+             <div class="custom-file">
+                <input type="file" class="custom-file-input" id="speedSVG_end">
+                <label class="custom-file-label" for="speedSVG_end">Choose file</label>
+             </div>
+             <div class="custom-file">
+                <input type="file" class="custom-file-input" id="defenseSVG_end">
+                <label class="custom-file-label" for="defenseSVG_end">Choose file</label>
+             </div>
+             <div class="custom-file">
+                <input type="file" class="custom-file-input" id="dexteritySVG_end">
+                <label class="custom-file-label" for="dexteritySVG_end">Choose file</label>
              </div>
 
     			  </div>
