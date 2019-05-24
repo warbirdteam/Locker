d18f64154ccdbe2174422ce7443126a0fc20d00c
Heasley <heasleys4hemp@hotmail.com>
2019-05-23 21:02:46 -0400

    more edits for inputs

--- chain-energy.php
+++ chain-energy.php
@@ -12,7 +12,7 @@ include('navbar.php');
 
     	<div class="row pb-3">
 
-    		 <div class="col-lg-6 col-md-6 pt-3">
+    		 <div class="col-lg-4 col-md-6 pt-3">
     			<div class="card border border-dark shadow rounded">
     			  <h5 class="card-header">Energy Contributions</h5>
     			  <div class="card-body">
@@ -27,16 +27,18 @@ include('navbar.php');
                 <input type="file" class="custom-file-input" name="speedSVG_start">
                 <label class="custom-file-label" for="speedSVG_start">Speed SVG</label>
              </div>
-             <div class="custom-file pb-2">
+             <div class="pb-2">
+             <div class="custom-file">
                 <input type="file" class="custom-file-input" name="defenseSVG_start">
                 <label class="custom-file-label" for="defenseSVG_start">Defense SVG</label>
              </div>
+            </div>
              <div class="custom-file pb-2">
                 <input type="file" class="custom-file-input" name="dexteritySVG_start">
                 <label class="custom-file-label" for="dexteritySVG_start">Dexterity SVG</label>
              </div>
          </form>
-             <hr/>
+             <br/><hr/><br/>
              <h5 class="title">Energy Reports: End of Chain</h5>
          <form action="" method="post" enctype="multipart/form-data">
              <div class="custom-file pb-2">
@@ -56,14 +58,14 @@ include('navbar.php');
                 <label class="custom-file-label" for="dexteritySVG_end">Dexterity SVG</label>
              </div>
          </form>
-             <button type="submit" class="btn btn-primary" name="importSubmit" value="IMPORT">Submit</button>
+             <button type="submit" class="btn btn-primary pt-3" name="importSubmit" value="IMPORT">Submit</button>
     			  </div>
     			</div>
     		 </div> <!-- col -->
       </div> <!-- row -->
 
       <div class="row">
-        <div class="col-lg-8 col-md-6 pt-3">
+        <div class="col-lg-6 col-md-6 pt-3">
           <div class="card border border-dark shadow rounded">
     			  <h5 class="card-header">Results</h5>
     			  <div class="card-body">
