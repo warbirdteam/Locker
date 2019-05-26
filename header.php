6950162eccccd9d48fda79810214265c9418bafd
Vulture <cw41182@gmail.com>
2019-05-26 06:30:16 -0500

    Updated login form
    More mobile friendly and nicer looking

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 	<!-- CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
 	<!-- jQuery -->
 	<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
--- index.php
+++ index.php
@@ -2,34 +2,36 @@
 include('header.php');
 include_once("../../../db_connect.php");
 ?>
-<title>Shitty Logon</title>
+<title>Login</title>
 <script type="text/javascript" src="script/jquery.validate.js"></script>
 <script type="text/javascript" src="script/login.js"></script>
 <link href="css/style.css" rel="stylesheet" type="text/css" media="screen">
 <?php include('navbar-index.php');?>
-<div class="container">
+<div class="content">
 
 <div class="container">
   <div class="row">
-   <div class="col-md-3"></div>
-   <div class="col-md-6 border border-dark shadow py-4 mt-4 rounded" style="background-color: #d2dae2;">
+   <div class="col-xl-4 col-lg-5 col-md-6 col-sm-7 col-8 border border-dark shadow mx-auto align-self-center py-4 mt-4 rounded">
      <form method="post" id="login-form">
-      <h2>User Log In Form</h2>
+	 <div>
+      <h2 class="font-weight-light text-center pb-2">Login</h2>
   <hr/>
-       <div class="form-group">
+       <div class="form-group pb-1">
          <input type="email" class="form-control" name="user_email" id="user_email" placeholder="Email Address">
 	<span id="check-e"></span>
        </div>
-       <div class="form-group">
+       <div class="form-group pb-1">
          <input type="password" class="form-control" name="password" id="password" placeholder="Password">
        </div>
   <hr />
+  <div class="footer text-center pt-2">
        <button type="submit" class="btn btn-primary" name="login_button" id="login_button">
          <i class="fas fa-sign-in-alt"></i> Sign In
        </button>
+	   </div>
+	 </div>
     </form>
   </div>
- <div class="col-md-3"></div>
 </div>
 </div>
 
