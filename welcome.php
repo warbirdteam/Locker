eae2fbdef346225ae0a64519e5b17e2d8b3655c6
Heasley <heasleys4hemp@hotmail.com>
2019-05-30 04:01:26 -0400

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -132,17 +132,6 @@ $('.canvasjs-chart-credit').hide();
 
 		 <div class="col-lg-3 col-md-6 pt-3">
 			<div class="card border border-dark shadow rounded">
-			  <h5 class="card-header">Placeholder</h5>
-			  <div class="card-body">
-
-			   <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-			  </div>
-			</div>
-		 </div> <!-- col -->
-
-		 <div class="col-lg-3 col-md-6 pt-3">
-			<div class="card border border-dark shadow rounded">
 			  <h5 class="card-header">Everything</h5>
 			  <div class="card-body">
 
