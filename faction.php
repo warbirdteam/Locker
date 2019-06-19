117e6f815dacbbeb044757f62ba325bef9b807d0
RelaxAndTakeNotes <cw41182@gmail.com>
2019-06-19 23:22:53 +0000

    STOP CHANGING STUFF ON THE SERVER. USE GITHUB ON YOUR DESKTOP YOU BUTT HOLE

--- faction.php
+++ faction.php
@@ -47,7 +47,7 @@ switch ($action) {
 
 $apikey = "UtuyM2roWM6vDjKj"; //Heasleys4hemp's apikey
 //$jsonurl = "https://api.torn.com/faction/13784?selections=basic&key=jIirMCNvK8q2hf8u";
-$jsonurl = "https://api.torn.com/user/?selections=timestamp,networth,bazaar,display,inventory,hof,travel,education,medals,honors,notifications,personalstats,workstats,crimes,icons,cooldowns,money,perks,battlestats,bars,profile,basic,stocks,properties,jobpoints,merits,refills,discord,gym&key=" . $apikey;
+$jsonurl = "https://api.torn.com/user/?selections=timestamp,networth,bazaar,display,inventory,hof,travel,education,medals,honors,notifications,personalstats,workstats,crimes,icons,cooldowns,money,perks,battlestats,bars,profile,basic,stocks,properties,jobpoints,merits,refills,discord,gym&key=" . $_SESSION['key'];
    $json = file_get_contents($jsonurl); //gets output of API
 
 $data = json_decode($json, true);
