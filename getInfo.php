ed402165f8480236b3af020d1153285304b95ed9
Heasley <heasleys4hemp@hotmail.com>
2019-06-20 04:04:24 -0400

    getinfo testing

---
+++ getInfo.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once("../../../db_connect.php");
+
+$result = $conn->query("SELECT users.tornuserkey FROM users WHERE tornuserkey <> 'testkey'");
+
+if($result === false) {
+    user_error("Query failed: ".$conn->error."\n$query");
+    return false;
+else {
+    if($result->num_rows > 0){
+      while($row = $result->fetch_assoc()){
+       $apikey = $row['tornuserkey'];
+       $jsonurl = "https://api.torn.com/user/?selections=timestamp,networth,bazaar,display,inventory,hof,travel,education,medals,honors,notifications,personalstats,workstats,crimes,icons,cooldowns,money,perks,battlestats,bars,profile,basic,stocks,properties,jobpoints,merits,refills,discord,gym&key=" . $apikey;
+       $json = file_get_contents($jsonurl);
+       $data = json_decode($json, true);
+
+       if ($data['error']) {echo 'Error';}
+       else{
+            if ($data['timestamp']) {
+                conn->query("INSERT INTO current_data (userid, name, energy, cooldown_drug, cooldown_booster, refill_energy, refill_nerve) VALUES ('".$data["player_id"]."', '".$data["name"]."', '".$data["energy"]["current"].'/'.$data["energy"]["maximum"]."', '".$data["cooldowns"]["drug"]."', '".$data["cooldowns"]["booster"]."', '".$data["refills"]["energy_refill_used"]."', '".$data["refills"]["nerve_refill_used"]."')");
+                echo 'done';
+            }
+
+          header("Location: faction.php");
+?>
