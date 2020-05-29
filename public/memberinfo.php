19161bcf14a3ea16b18f5fb544972458706acf43
Heasley <Heasleys4hemp@hotmail.com>
2020-05-28 22:51:49 -0400

    Resign Member Info
    Added buttons instead of tabs, load info dynamically instead of all at once

--- public/memberinfo.php
+++ public/memberinfo.php
@@ -8,26 +8,26 @@ include('includes/header.php');
 
 
 <?php
-	switch ($_SESSION['role']) {
-	    case 'admin':
-	        include('includes/navbar-admin.php');
-	        break;
-	    case 'leadership':
-	        include('includes/navbar-leadership.php');
-	        break;
-	    case 'member':
-	        include('includes/navbar-member.php');
-	        break;
-	    case 'guest':
-          $_SESSION['error'] = "You do not have access to that area.";
-	        header("Location: /welcome.php");
-	        break;
-    default:
-        $_SESSION = array();
-        $_SESSION['error'] = "You are no longer logged in.";
-        header("Location: /index.php");
-        break;
-	}
+switch ($_SESSION['role']) {
+	case 'admin':
+	include('includes/navbar-admin.php');
+	break;
+	case 'leadership':
+	include('includes/navbar-leadership.php');
+	break;
+	case 'member':
+	include('includes/navbar-member.php');
+	break;
+	case 'guest':
+	$_SESSION['error'] = "You do not have access to that area.";
+	header("Location: /welcome.php");
+	break;
+	default:
+	$_SESSION = array();
+	$_SESSION['error'] = "You are no longer logged in.";
+	header("Location: /index.php");
+	break;
+}
 
 // Load classes
 include_once(__DIR__ . "/../includes/autoloader.inc.php");
@@ -41,183 +41,95 @@ include_once(__DIR__ . "/../includes/autoloader.inc.php");
 				<h5 class="card-header">Member Information</h5>
 				<div class="card-body">
 
-          <ul class="nav nav-tabs" id="memberTabs" role="tablist">
-    				<li class="nav-item">
-    				<a class="nav-link active" id="nest-members-week-tab" data-toggle="tab" href="#week-35507" role="tab">Nest: 7 Days</a>
-    				</li>
-            <li class="nav-item">
-    				<a class="nav-link" id="nest-members-month-tab" data-toggle="tab" href="#month-35507" role="tab">Nest: 30 Days</a>
-    				</li>
-    				<li class="nav-item">
-    				<a class="nav-link" id="wb-members-week-tab" data-toggle="tab" href="#week-13784" role="tab">Warbirds: 7 Days</a>
-    				</li>
-            <li class="nav-item">
-    				<a class="nav-link" id="wb-members-month-tab" data-toggle="tab" href="#month-13784" role="tab">Warbirds: 30 Days</a>
-    				</li>
-    				<li class="nav-item">
-    				<a class="nav-link" id="wbng-members-week-tab" data-toggle="tab" href="#week-30085" role="tab">WBNG: 7 Days</a>
-    				</li>
-            <li class="nav-item">
-    				<a class="nav-link" id="wbng-members-month-tab" data-toggle="tab" href="#month-30085" role="tab">WBNG: 30 Days</a>
-    				</li>
-    			</ul>
-	<div class="tab-content" id="memberTabsContent">
+					<ul class="nav nav-pills nav-justified flex-column flex-md-row my-2" id="memberTabs" role="tablist">
+						<li class="nav-item mx-2 mb-2">
+							<a class="flex-md-fill nav-link border border-dark" id="nest-members-week-tab" data-fid="35507" data-timeline="week" data-toggle="tab" href="#week-35507" role="tab">Nest: 7 Days</a>
+						</li>
+						<li class="nav-item  mx-2 mb-2">
+							<a class="flex-md-fill nav-link border border-dark" id="nest-members-month-tab" data-fid="35507" data-timeline="month" data-toggle="tab" href="#month-35507" role="tab">Nest: 30 Days</a>
+						</li>
+						<li class="nav-item  mx-2 mb-2">
+							<a class="flex-md-fill nav-link border border-dark" id="wb-members-week-tab" data-fid="13784" data-timeline="week" data-toggle="tab" href="#week-13784" role="tab">Warbirds: 7 Days</a>
+						</li>
+						<li class="nav-item  mx-2 mb-2">
+							<a class="flex-md-fill nav-link border border-dark" id="wb-members-month-tab" data-fid="13784" data-timeline="month" data-toggle="tab" href="#month-13784" role="tab">Warbirds: 30 Days</a>
+						</li>
+						<li class="nav-item  mx-2 mb-2">
+							<a class="flex-md-fill nav-link border border-dark" id="wbng-members-week-tab" data-fid="30085" data-timeline="week" data-toggle="tab" href="#week-30085" role="tab">WBNG: 7 Days</a>
+						</li>
+						<li class="nav-item  mx-2 mb-2">
+							<a class="flex-md-fill nav-link border border-dark" id="wbng-members-month-tab" data-fid="30085" data-timeline="month" data-toggle="tab" href="#month-30085" role="tab">WBNG: 30 Days</a>
+						</li>
+					</ul>
+					<div class="tab-content" id="memberTabsContent">
+						<?php
+
+						$factions = array( "35507", "13784", "30085" );
+
+						foreach ($factions as $faction) { ?>
+
+
+							<div class="tab-pane fade" id="week-<?php echo $faction;?>" role="tabpanel">
+								<div class="table-responsive">
+
+									<div class="d-flex justify-content-center mt-2">
+										<div class="spinner-grow spinner-grow-sm" role="status">
+											<span class="sr-only">Loading...</span>
+										</div>
+										<div class="spinner-grow spinner-grow-sm" role="status">
+										</div>
+										<div class="spinner-grow spinner-grow-sm" role="status">
+										</div>
+										<div class="spinner-grow spinner-grow-sm" role="status">
+										</div>
+										<div class="spinner-grow spinner-grow-sm" role="status">
+										</div>
+									</div>
+
+								</div>
+
+							</div>
+
+
+							<div class="tab-pane fade" id="month-<?php echo $faction;?>" role="tabpanel">
+								<div class="table-responsive">
+
+									<div class="d-flex justify-content-center mt-2">
+										<div class="spinner-grow spinner-grow-sm" role="status">
+											<span class="sr-only">Loading...</span>
+										</div>
+										<div class="spinner-grow spinner-grow-sm" role="status">
+										</div>
+										<div class="spinner-grow spinner-grow-sm" role="status">
+										</div>
+										<div class="spinner-grow spinner-grow-sm" role="status">
+										</div>
+										<div class="spinner-grow spinner-grow-sm" role="status">
+										</div>
+									</div>
+
+								</div>
+
+							</div>
+							<?php
+						}//foreach faction
+						?>
+
+
+
+
+
+
+					</div>
+
+				</div> <!-- card-body -->
+			</div> <!-- card -->
+		</div> <!-- col -->
+	</div> <!-- row -->
+
+
+</div> <!-- container -->
+<script type="text/javascript" src="js/memberinfo.js"></script>
 <?php
-
-$factions = array( "35507", "13784", "30085" );
-
-foreach ($factions as $faction) { ?>
-
-
-		<div class="tab-pane fade<?php if($faction == '35507'){echo ' show active';}?>" id="week-<?php echo $faction;?>" role="tabpanel">
-      <div class="table-responsive">
-			<table class="table table-hover table-striped table-dark table-fit table-sm" border=1>
-        <thead class="thead-dark">
-					<tr>
-						<th scope="col" class="text-truncate">Name</th>
-						<th scope="col" class="text-truncate">Donator</th>
-						<th scope="col" class="text-truncate">Property</th>
-						<th scope="col" class="text-truncate">Last Action</th>
-            <th scope="col" class="text-truncate">XanScore<i class="far fa-copyright"></i></th>
-						<th scope="col" class="text-truncate">Xanax</th>
-						<th scope="col" class="text-truncate">ODs</th>
-						<th scope="col" class="text-truncate">Energy Rx</th>
-						<th scope="col" class="text-truncate">Nerve Rx</th>
-						<th scope="col" class="text-truncate">Boosters</th>
-						<th scope="col" class="text-truncate">Cans</th>
-						<th scope="col" class="text-truncate">SEs</th>
-						<th scope="col" class="text-truncate">Travels</th>
-						<th scope="col" class="text-truncate">Dump</th>
-					</tr>
-				</thead>
-			<tbody>
-<?php
-			$db_member_list = new DB_request();
-			$rows = $db_member_list->getFactionMembersByFaction($faction);
-			$count = $db_member_list->row_count;
-
-			if($count > 0){
-				$counter = 0;
-				foreach ($rows as $row){
-
-
-
-					$db_memberinfo = new DB_request();
-					$data = $db_memberinfo->getMemberInfoByIDWeek($row['userid']);
-					$membercount = $db_memberinfo->row_count;
-
-          if ($membercount > 0){
-							$counter++;
-							$lastactionclass = ($row['last_action'] <= strtotime('-24 hours')) ? 'class="fit bg-danger"' : 'class="fit"';
-							$title = round((time() - $row['last_action'])/60/60);
-							$title .= ' hours ago';
-
-							$propertyclass = strpos($data[0]["property"],'Private Island') !== false ? 'class="fit"' : 'class="fit bg-danger"';
-
-							$donatorclass = $data[0]['donator'] == 0 ? 'class="fit bg-danger"' : 'class="fit"';
-
-							echo '<tr><td class="fit"><a class="text-reset" href="https://www.torn.com/profiles.php?XID=' . $data[0]["userid"] . '" target="_blank">' . $row["name"] . ' [' . $data[0]["userid"] . ']</a></td><td ' . $donatorclass . '>'  . $data[0]["donator"] . '</td><td ' . $propertyclass . '>'. $data[0]["property"] . '</td><td data-toggle="tooltip" data-placement="left" title="'.$title.'" '. $lastactionclass .'>'. date('m-d-Y H:i:s',$row["last_action"]) . '</td><td class="fit">'.number_format((float)$data[0]["xanscore"], 2, '.', '').'</dt><td class="fit">'.$data[0]["xanaxweek"].'</td><td class="fit">'.$data[0]["overdosedweek"].'</td><td class="fit">'.$data[0]["refill_energyweek"].'</td><td class="fit">'.$data[0]["refill_nerveweek"].'</td><td class="fit">'.$data[0]["boostersusedweek"].'</td><td class="fit">'.$data[0]["energydrinkusedweek"].'</td><td class="fit">'.$data[0]["statenhancersusedweek"].'</td><td class="fit">'.$data[0]["travelweek"].'</td><td class="fit">'.$data[0]["dumpsearchesweek"].'</td></tr>';
-					}
-				}
-			} else {
-				echo '<tr><td colspan=14 align=center>No members found...</td></tr>';
-			}
+include('includes/footer.php');
 ?>
-			</tbody>
-				<tfoot>
-					<tr>
-						<td colspan=14 align=center>Total: <?php echo $counter . '/' . $count;?> </td>
-					</tr>
-				</tfoot>
-			</table>
-		  </div>
-
-		</div>
-
-
-		<div class="tab-pane fade" id="month-<?php echo $faction;?>" role="tabpanel">
-			<div class="table-responsive">
-			<table class="table table-hover table-striped table-dark table-fit table-sm" border=1>
-				<thead class="thead-dark">
-					<tr>
-						<th scope="col" class="text-truncate">Name</th>
-						<th scope="col" class="text-truncate">Donator</th>
-						<th scope="col" class="text-truncate">Property</th>
-						<th scope="col" class="text-truncate">Last Action</th>
-            <th scope="col" class="text-truncate">XanScore<i class="far fa-copyright"></i></th>
-						<th scope="col" class="text-truncate">Xanax</th>
-						<th scope="col" class="text-truncate">ODs</th>
-						<th scope="col" class="text-truncate">Energy Rx</th>
-						<th scope="col" class="text-truncate">Nerve Rx</th>
-						<th scope="col" class="text-truncate">Boosters</th>
-						<th scope="col" class="text-truncate">Cans</th>
-						<th scope="col" class="text-truncate">SEs</th>
-						<th scope="col" class="text-truncate">Travels</th>
-						<th scope="col" class="text-truncate">Dump</th>
-					</tr>
-				</thead>
-			<tbody>
-<?php
-			$db_member_list = new DB_request();
-			$rows = $db_member_list->getFactionMembersByFaction($faction);
-			$count = $db_member_list->row_count;
-
-			if($count > 0){
-				$counter = 0;
-				foreach ($rows as $row){
-
-
-
-					$db_memberinfo = new DB_request();
-					$data = $db_memberinfo->getMemberInfoByIDMonth($row['userid']);
-					$membercount = $db_memberinfo->row_count;
-
-          if ($membercount > 0){
-							$counter++;
-							$lastactionclass = ($row['last_action'] <= strtotime('-24 hours')) ? 'class="fit bg-danger"' : 'class="fit"';
-							$title = round((time() - $row['last_action'])/60/60);
-							$title .= ' hours ago';
-
-							$propertyclass = strpos($data[0]["property"],'Private Island') !== false ? 'class="fit"' : 'class="fit bg-danger"';
-
-							$donatorclass = $data[0]['donator'] == 0 ? 'class="fit bg-danger"' : 'class="fit"';
-
-							echo '<tr><td class="fit"><a class="text-reset" href="https://www.torn.com/profiles.php?XID=' . $data[0]["userid"] . '" target="_blank">' . $row["name"] . ' [' . $data[0]["userid"] . ']</a></td><td ' . $donatorclass . '>'  . $data[0]["donator"] . '</td><td ' . $propertyclass . '>'. $data[0]["property"] . '</td><td data-toggle="tooltip" data-placement="left" title="'.$title.'" '. $lastactionclass .'>'. date('m-d-Y H:i:s',$row["last_action"]) . '</td><td class="fit">'.number_format((float)$data[0]["xanscore"], 2, '.', '').'</dt><td class="fit">'.$data[0]["xanaxweek"].'</td><td class="fit">'.$data[0]["overdosedweek"].'</td><td class="fit">'.$data[0]["refill_energyweek"].'</td><td class="fit">'.$data[0]["refill_nerveweek"].'</td><td class="fit">'.$data[0]["boostersusedweek"].'</td><td class="fit">'.$data[0]["energydrinkusedweek"].'</td><td class="fit">'.$data[0]["statenhancersusedweek"].'</td><td class="fit">'.$data[0]["travelweek"].'</td><td class="fit">'.$data[0]["dumpsearchesweek"].'</td></tr>';
-					}
-				}
-			} else {
-				echo '<tr><td colspan=14 align=center>No members found...</td></tr>';
-			}
-?>
-			</tbody>
-				<tfoot>
-					<tr>
-						<td colspan=14 align=center>Total: <?php echo $counter . '/' . $count;?></td>
-					</tr>
-				</tfoot>
-			</table>
-		  </div>
-
-		</div>
-<?php
-}//foreach faction
-?>
-
-
-
-
-
-
-          </div>
-
-        </div> <!-- card-body -->
-       </div> <!-- card -->
-      </div> <!-- col -->
-    </div> <!-- row -->
-
-
-  </div> <!-- container -->
-
-  <?php
-  include('includes/footer.php');
-  ?>
---
+++ public/process/getMemberInfo.php
@@ -0,0 +1,126 @@
+<?php
+//##### MEMBER & LEADERSHIP & ADMIN ONLY PAGE
+session_start();
+
+switch ($_SESSION['role']) {
+  case 'admin':
+  //continue
+  break;
+  case 'leadership':
+  //continue
+  break;
+  case 'member':
+  //continue
+  break;
+  case 'guest':
+  $_SESSION['error'] = "You do not have access to that.";
+  header("Location: /welcome.php");
+  break;
+  default:
+  $_SESSION = array();
+  $_SESSION['error'] = "You are no longer logged in.";
+  header("Location: /index.php");
+  break;
+}
+
+include_once(__DIR__ . "/../../includes/autoloader.inc.php");
+
+
+if (isset($_POST["fid"])) {
+  $fid = $_POST["fid"];
+  if (isset($_POST["timeline"])) {
+    $timeline = $_POST["timeline"];
+  } else {$error = new Error_Message("Something went wrong with member information lookup.","../welcome.php");}
+} else {$error = new Error_Message("Something went wrong with member information lookup.","../welcome.php");}
+
+switch ($fid) {
+
+  case '35507':
+  memberInfo('35507', $timeline);
+  break;
+  case '13784':
+  memberInfo('13784', $timeline);
+  break;
+  case '30085':
+  memberInfo('30085', $timeline);
+  break;
+
+  default:
+  $error = new Error_Message("That was not a proper faction.","../welcome.php");
+  break;
+}
+
+function memberInfo($faction, $timeline) {
+  $tabledata = '';
+  echo '<table class="table table-hover table-striped table-dark table-sm" border=1 id="table_' . $faction . '_' . $timeline . '"><thead class="thead-dark">
+  <tr>
+  <th scope="col" class="text-truncate sorter-false">#</th>
+  <th scope="col" class="text-truncate">Name</th>
+  <th scope="col" class="text-truncate">Donator</th>
+  <th scope="col" class="text-truncate">Property</th>
+  <th scope="col" class="text-truncate">Last Action</th>
+  <th scope="col" class="text-truncate">XanScore<i class="far fa-copyright"></i></th>
+  <th scope="col" class="text-truncate">Xanax</th>
+  <th scope="col" class="text-truncate">ODs</th>
+  <th scope="col" class="text-truncate">Energy Rx</th>
+  <th scope="col" class="text-truncate">Nerve Rx</th>
+  <th scope="col" class="text-truncate">Boosters</th>
+  <th scope="col" class="text-truncate">Cans</th>
+  <th scope="col" class="text-truncate">SEs</th>
+  <th scope="col" class="text-truncate">Travels</th>
+  <th scope="col" class="text-truncate">Dump</th>
+  </tr>
+  </thead><tbody>';
+
+  $db_member_list = new DB_request();
+  $rows = $db_member_list->getFactionMembersByFaction($faction);
+  $count = $db_member_list->row_count;
+
+  if($count > 0){
+    $counter = 0;
+    foreach ($rows as $row){
+
+
+
+      $db_memberinfo = new DB_request();
+      if ($timeline == 'week') {
+        $data = $db_memberinfo->getMemberInfoByIDWeek($row['userid']);
+        $membercount = $db_memberinfo->row_count;
+      }
+      if ($timeline == 'month') {
+        $data = $db_memberinfo->getMemberInfoByIDMonth($row['userid']);
+        $membercount = $db_memberinfo->row_count;
+      }
+
+      if ($membercount > 0){
+        $counter++;
+
+        $title = round((time() - $row['last_action'])/60/60);
+        $title .= ' hours ago';
+
+
+        if ($row['name'] == 'Geometroid') {
+          $fallenclass = ' class="bg-info"';
+          $lastactionclass = 'class="bg-info"';
+          $propertyclass = 'class="bg-info"';
+          $donatorclass = 'class="bg-info"';
+        } else {
+          $fallenclass = '';
+
+          $lastactionclass = ($row['last_action'] <= strtotime('-24 hours')) ? 'class="bg-danger"' : '';
+          $propertyclass = strpos($data[0]["property"],'Private Island') !== false ? '' : 'class="bg-danger"';
+          $donatorclass = $data[0]['donator'] == 0 ? 'class="bg-danger"' : '';
+
+        }
+
+        $tabledata .= '<tr'.$fallenclass.'><td></td><td><a class="text-reset" href="https://www.torn.com/profiles.php?XID=' . $data[0]["userid"] . '" target="_blank">' . $row["name"] . ' [' . $data[0]["userid"] . ']</a></td><td ' . $donatorclass . '>'  . $data[0]["donator"] . '</td><td ' . $propertyclass . '>'. $data[0]["property"] . '</td><td data-toggle="tooltip" data-placement="left" title="'.$title.'" '. $lastactionclass .'>'. date('m-d-Y H:i:s',$row["last_action"]) . '</td><td>'.number_format((float)$data[0]["xanscore"], 2, '.', '').'</dt><td>'.$data[0]["xanaxweek"].'</td><td>'.$data[0]["overdosedweek"].'</td><td>'.$data[0]["refill_energyweek"].'</td><td>'.$data[0]["refill_nerveweek"].'</td><td>'.$data[0]["boostersusedweek"].'</td><td>'.$data[0]["energydrinkusedweek"].'</td><td>'.$data[0]["statenhancersusedweek"].'</td><td>'.$data[0]["travelweek"].'</td><td>'.$data[0]["dumpsearchesweek"].'</td></tr>';
+      }
+    }
+
+    echo $tabledata;
+    echo '</tbody><tfoot><tr><td colspan=15 align=center>Total: ' . $counter . '/' . $count . '</td></tr></tfoot></table>';
+  } else {
+    echo '<tr><td colspan=15 align=center>No members found...</td></tr></table>';
+  }
+
+}
