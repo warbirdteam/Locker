1b43620040fd60605cf32d3938f5923941961ebf
Heasley <heasleys4hemp@hotmail.com>
2020-11-18 23:54:43 -0500

    upload warbase script chain ids

---
+++ public/process/uploadChainID.php
@@ -0,0 +1,23 @@
+<?php
+
+$userID = isset($_GET["userID"]) && is_numeric($_GET["userID"]) && strlen($_GET["userID"]) <= 10 ? $_GET["userID"] : 'NULL';
+$factionID = isset($_GET["factionID"]) && is_numeric($_GET["factionID"]) && strlen($_GET["factionID"]) <= 10 ? $_GET["factionID"] : 'NULL';
+$chainID = isset($_GET["chainID"]) && is_numeric($_GET["chainID"]) && strlen($_GET["chainID"]) <= 10 ? $_GET["chainID"] : 'NULL';
+
+if ($factionID == "NULL" OR $chainID == "NULL" OR $userID == "NULL") {
+  return "failure to upload data";
+}
+
+include_once("../../includes/autoloader.inc.php");
+
+$db_upload_chain = new db_request();
+$row = $db_upload_chain->getChainByChainID($chainID);
+
+if ($row != 'NULL') {
+  $db_upload_chain->insertChainID($factionID,$chainID,$userID);
+  return "successfully uploaded chain id";
+} else {
+  return "chain already in database";
+}
+
+ ?>
