f52ad98ab3619d3ff73682df1df85bb87903fbc9
Heasley <heasleys4hemp@hotmail.com>
2021-09-29 01:03:05 -0400

    modify permission access

--- public/process/addEnemyFaction.php
+++ public/process/addEnemyFaction.php
@@ -1,19 +1,23 @@
 <?php
+//start the session array
 session_start();
+//If cannot find site ID, empty session array and send to login page with error message
 if(!isset($_SESSION['siteID'])){
-  $_SESSION = array();
+	$_SESSION = array();
 	$_SESSION['error'] = "You are no longer logged in.";
-	header("Location: ../index.php");
-  exit;
+	header("Location: /index.php");
+	exit;
 }
-
-if ($_SESSION['roleValue'] <= 2) { // 1 = guest / register, 2 = member, 3 = leadership, 4 = admin
+if ($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'leadership') {
+	//##### LEADERSHIP & ADMIN ONLY PAGE
+  //load classes files in classes folder
+  include_once(__DIR__ . "/../../includes/autoloader.inc.php");
+} else {
+	//else send to welcome page with error message
 	$_SESSION['error'] = "You do not have access to that area.";
 	header("Location: /welcome.php");
   exit;
 }
-
-include_once(__DIR__ . "/../../includes/autoloader.inc.php");
 
 if (isset($_POST['fidEnemy']) && !empty($_POST['fidEnemy']) && is_numeric($_POST['fidEnemy'])) {
     $fid = $_POST['fidEnemy'];
