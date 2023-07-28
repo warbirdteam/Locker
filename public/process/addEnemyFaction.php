b01da2c94bbf1d06204d4428fe56fa2abb4e5963
Heasley <heasleys4hemp@hotmail.com>
2023-07-27 22:18:53 -0400

    Update addEnemyFaction.php

--- public/process/addEnemyFaction.php
+++ public/process/addEnemyFaction.php
@@ -58,7 +58,7 @@ if (isset($_POST['fidEnemy']) && !empty($_POST['fidEnemy'])) {
           $db_request_add_enemy_faction = new db_request();
           $db_request_add_enemy_faction->insertEnemyFactionInfo($fid, $fname, $leader, $coleader, $age, $best_chain, $total_members, $respect, $timestamp);
     
-          include(__DIR__ . "/../../misc/getEnemyFactionMembers.php");
+          getEnemyFactionMembers('1468764', $fid);
         } //if faction api exists
       }
 
@@ -107,7 +107,8 @@ if (isset($_POST['fidEnemy']) && !empty($_POST['fidEnemy'])) {
       $db_request_add_enemy_faction = new db_request();
       $db_request_add_enemy_faction->insertEnemyFactionInfo($fid, $fname, $leader, $coleader, $age, $best_chain, $total_members, $respect, $timestamp);
 
-      include_once(__DIR__ . "/../../misc/getEnemyFactionMembers.php");
+      getEnemyFactionMembers('1468764', $fid);
+
 
       $_SESSION['success'] = "Enemy Faction <b>". $fname . " [" . $fid . "]</b> added.";
       header("Location: ../war.php");
@@ -118,4 +119,86 @@ if (isset($_POST['fidEnemy']) && !empty($_POST['fidEnemy'])) {
 $_SESSION['error'] = "The faction you entered does not exist.";
 header("Location: ../war.php");
 exit;
+
+
+function getEnemyFactionMembers($userid, $factionID) {
+  $db_request = new db_request();
+  $apikey = $db_request->getRawAPIKeyByUserID($userid);
+
+  $db_request_check_enemy_faction = new db_request();
+  $row = $db_request_check_enemy_faction->getEnemyFactionByFactionID($factionID);
+
+  if (!empty($row)) {
+    //faction is already in list of enemy factions
+    $factionTimestamp = $row['timestamp'];
+    $now = time();
+    if($now >= ($factionTimestamp + 3600)) {
+      //continue to gather faction data
+    } else {
+      return; //skip api pull and update because it was recently updated an hour ago
+    }
+  }
+
+
+  $api_request = new api_request($apikey);
+  $factionData = $api_request->getFactionAPI($factionID);
+
+  //save faction info to database
+  $fid = $factionData['ID'];
+  $fname = $factionData['name'];
+  $leader = $factionData['leader'];
+  $coleader = $factionData['co-leader'];
+  $age = $factionData['age'];
+  $best_chain = $factionData['best_chain'];
+  $total_members = count($factionData['members']);
+  $respect = $factionData['respect'];
+  $timestamp = time();
+
+  $row = $db_request->getEnemyFactionByFactionID($fid);
+
+  if($row) {
+    $db_request->updateEnemyFactionInfo($fid, $fname, $leader, $coleader, $age, $best_chain, $total_members, $respect, $timestamp);
+  } else {
+    $db_request->insertEnemyFactionInfo($fid, $fname, $leader, $coleader, $age, $best_chain, $total_members, $respect, $timestamp);
+  }
+
+
+  $members = $factionData['members']; //members array
+
+  $dbMemberData = $db_request->getEnemyMembersByFaction($fid);
+
+  if(!empty($dbMemberData) && !empty($members)) {
+
+    $diff = array_diff_key($dbMemberData, $members);
+
+    //delete member from database if exists in diff array
+    while ($cut = current($diff)) {
+      $cutuser = key($diff);
+      $memberData = $db_request->getEnemyMemberByTornID($cutuser);
+      if ($memberData) {
+        $db_request->removeEnemyMemberByTornID($cutuser);
+      }
+      next($diff);
+    } //while
+
+  }
+
+
+
+  while ($member = current($members)) {
+    $userid = key($members);
+    $row = $db_request->getEnemyMemberByTornID($userid);
+
+    if($row) {
+      $db_request->updateEnemyMember($userid, $member);
+    } else {
+      $db_request->insertEnemyMember($userid, $fid, $member);
+    }
+
+    next($members);
+  }
+
+} //getEnemyFactionMembers Function
+
+
 ?>
