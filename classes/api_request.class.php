516452d0fcd9a46258c5719efe65a0e2e5f97ca8
Heasley <heasleys4hemp@hotmail.com>
2024-09-11 08:45:27 -0400

    better api error handling and update key availability

--- classes/api_request.class.php
+++ classes/api_request.class.php
@@ -3,17 +3,22 @@
 class api_request {
 
   protected $apikey;
+  private $siteID;
   private $baseURL = "https://api.torn.com";
   private $baseURLv2 = "https://api.torn.com/v2";
 
   /////////////////////////////////////////////////
 
-  public function __construct($apikey) {
+  public function __construct($apikey, $siteID = null) {
 
     if ( isset($apikey) && !empty($apikey) ) {
       $this->apikey = $apikey;
     } else {
       throw new Exception('API Key was not passed correctly or does not exist.');
+    }
+
+    if ( isset($siteID) && !empty($siteID) ) {
+      $this->siteID = $siteID;
     }
 
     if ( !$this->verifyString() ) {
@@ -59,6 +64,7 @@ class api_request {
       }
     } else {
       throw new Exception('Error while fetching API data. Data not in JSON form.');
+      return false;
     }
 
     return true;
@@ -137,13 +143,123 @@ class api_request {
     return $this->pullAPI('faction', $factionid, 'timestamp,rankedwars');
   }
 
-
+  /////////////////////////////////////////////////
+  ////////             API ERROR           ////////
   /////////////////////////////////////////////////
 
   private function APIError($error) {
     //Check Error Code and describe reason, such as bad key, etc
+    if (!isset($error['code'])) {
+      throw new Exception('API Error: No error code.');
+      exit('API Error: No error code. ');
+    }
+
+    switch ($error['code']) {
+      case 0:
+        //  Unknown error : Unhandled error, should not occur.
+      break;
+
+      case 1:
+        // Key is empty : Private key is empty in current request.
+      break;
+
+      case 2:
+        // Incorrect Key : Private key is wrong/incorrect format.
+        $this->updateKeyActive(0);
+      break;
+      
+      case 3:
+        // Wrong type : Requesting an incorrect basic type.
+      break;
+
+      case 4:
+        // Wrong fields : Requesting incorrect selection fields.
+      break;
+
+      case 5:
+        // Too many requests : Requests are blocked for a small period of time because of too many requests per user (max 100 per minute).
+      break;
+
+      case 6:
+        // Incorrect ID : Wrong ID value.
+      break;
+
+      case 7:
+        //  Incorrect ID-entity relation : A requested selection is private (For example, personal data of another user / faction).
+      break;
+
+      case 8:
+        // IP block : Current IP is banned for a small period of time because of abuse.
+      break;
+
+      case 9:
+        //  API disabled : Api system is currently disabled.
+      break;
+
+      case 10:
+        // Key owner is in federal jail : Current key can't be used because owner is in federal jail.
+        $this->updateKeyActive(0);
+      break;
+
+      case 11:
+        //  Key change error : You can only change your API key once every 60 seconds.
+      break;
+
+      case 12:
+        // Key read error : Error reading key from Database.
+      break;
+
+      case 13:
+        // The key is temporarily disabled due to owner inactivity : The key owner hasn't been online for more than 7 days.
+        $this->updateKeyActive(0);
+      break;
+
+      case 14:
+        // Daily read limit reached : Too many records have been pulled today by this user from our cloud services.
+      break;
+
+      case 15:
+        // Temporary error : An error code specifically for testing purposes that has no dedicated meaning.
+      break;
+
+      case 16:
+        // Access level of this key is not high enough : A selection is being called of which this key does not have permission to access.
+      break;
+
+      case 17:
+        // Backend error occurred, please try again.
+      break;
+
+      case 18:
+        // API key has been paused by the owner.
+        $this->updateKeyActive(0);
+      break;
+
+      case 19:
+        // Must be migrated to crimes 2.0.
+      break;
+
+      case 20:
+        // Race not yet finished.
+      break;
+
+      case 21:
+        // Incorrect category : Wrong cat value.
+      break;
+      
+      default:
+        exit('API Key Error Code: ' . $error['code'] . ' - ' . $error['error']);
+      break;
+    }
+
     throw new Exception('API Key Error Code: ' . $error['code'] . ' - ' . $error['error']);
-    exit('API Key Error Code: ' . $error['code'] . ' - ' . $error['error']);
+  }
+
+  private function updateKeyActive($active) {
+    if (isset($this->siteID) && !empty($this->siteID)) {
+      $db_request_api = new db_request();
+      $db_request_api->updateSiteAPIActive($this->siteID, $active);
+    }
   }
 
   /////////////////////////////////////////////////
